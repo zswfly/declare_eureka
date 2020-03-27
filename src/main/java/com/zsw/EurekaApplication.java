7244b10d31356afe8fdbfa078414d8c775067c30
zhangshaowei <zhangshaowei@iplas.com.cn>
2020-03-27 20:18:09 +0800

    new

---
+++ src/main/java/com/zsw/EurekaApplication.java
@@ -0,0 +1,14 @@
+package com.zsw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication(scanBasePackages = "com.zsw.**")
+@EnableEurekaServer
+public class EurekaApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+
+}
