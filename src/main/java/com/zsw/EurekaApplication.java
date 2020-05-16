713c3d11960629b0f5bb4e4d5c5951a5cd53dd45
zhangshaowei <zhangshaowei@iplas.com.cn>
2020-05-17 00:17:09 +0800

    new

--- src/main/java/com/zsw/EurekaApplication.java
+++ src/main/java/com/zsw/EurekaApplication.java
@@ -1,5 +1,6 @@
 package com.zsw;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
